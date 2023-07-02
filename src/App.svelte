8ee24a06c1054d623cdbc7422d5b1488ba12c4af
Exedice <except.dice@gmail.com>
2023-07-02 11:24:00 +0300

    Routing
    • Added routing
    • Added a note about the type errors for routing
    • Added the debug instructions to README

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,26 @@
-<script lang="ts">
-	
+<!-- NOTE: this component should be written in JavaScript, not TypeScript -->
+<!-- If TypeScript is used, the Router library shows a bunch of errors that aren't actually affecting the program -->
+
+<script>
+
+	import { Router, Link, Route } from "svelte-routing";
+	import LandingPage from "./routes/LandingPage.svelte";
+	import AboutPage from "./routes/AboutPage.svelte";
+	import ProjectsPage from "./routes/ProjectsPage.svelte";
+  
+	export let url = "/";
+
 </script>
-
-<main>
-
-	Hello, World!
-	
-</main>
-
-<style>
-	
-</style>
+  
+<Router {url}>
+	<nav>
+		<Link to="/">Home</Link>
+		<Link to="/about">About</Link>
+		<Link to="/projects">Blog</Link>
+	</nav>
+	<div>
+		<Route path="/" component={LandingPage} />
+		<Route path="/about" component={AboutPage} />
+		<Route path="/projects" component={ProjectsPage} />
+	</div>
+</Router>
